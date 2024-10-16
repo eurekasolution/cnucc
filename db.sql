4fe4b08f02c9b357ff2f5d9c1f8386560f227302
eurekasolution <kjmin@daum.net>
2024-10-16 11:59:49 +0900

    10-16 Fin

---
+++ db.sql
@@ -0,0 +1,79 @@
+-- MariaDB dump 10.19  Distrib 10.4.32-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: culture
+-- ------------------------------------------------------
+-- Server version	10.4.32-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `aaa`
+--
+
+DROP TABLE IF EXISTS `aaa`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `aaa` (
+  `id` char(20) DEFAULT NULL,
+  `name` char(20) DEFAULT NULL,
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `aaa`
+--
+
+LOCK TABLES `aaa` WRITE;
+/*!40000 ALTER TABLE `aaa` DISABLE KEYS */;
+INSERT INTO `aaa` VALUES ('test','테스트'),('kdhong','홍길동');
+/*!40000 ALTER TABLE `aaa` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `my_user_table`
+--
+
+DROP TABLE IF EXISTS `my_user_table`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `my_user_table` (
+  `idx` int(11) NOT NULL AUTO_INCREMENT,
+  `id` varchar(50) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `pass` varchar(255) NOT NULL,
+  `birth` date DEFAULT NULL,
+  PRIMARY KEY (`idx`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=174 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `my_user_table`
+--
+
+LOCK TABLES `my_user_table` WRITE;
+/*!40000 ALTER TABLE `my_user_table` DISABLE KEYS */;
+INSERT INTO `my_user_table` VALUES (1,'abcde','세종','1111','1567-05-15'),(2,'fghij','정조','1111','1745-08-28'),(3,'klmno','이순신','1111','1545-09-08'),(4,'pqrst','강감찬','1111','1582-12-12'),(6,'djjung','정도전','1111','1500-01-01'),(7,'efghi','윤봉길','1111','1908-06-21'),(8,'jklmn','안중근','1111','1879-09-02'),(9,'opqrs','유관순','1111','1902-12-16'),(10,'gkim','김구','1111','1876-08-29'),(11,'yzabc','허준','1111','1546-10-01'),(12,'defgh','신사임당','1111','1504-10-29'),(13,'ijklm','황희','1111','1363-02-03'),(14,'nopqr','안창호','1111','1878-11-09'),(15,'stuvw','장영실','1111','1390-07-01'),(16,'xyzaa','이방원','1111','1367-10-13'),(17,'bcdef','박지원','1111','1737-02-05'),(18,'ghijk','유성룡','1111','1542-09-12'),(19,'lmnop','신숙주','1111','1417-01-12'),(20,'qrstu','정약용','1111','1762-06-16'),(21,'kjmin','민경주','1111','2000-10-03'),(171,'aaa','bbb','$2y$10$KnqRe2Wy8rrLrkmTSbvd7uHzJsp.UlVjAuuIjK/oN7pCJDj2iUhnm','1500-01-12'),(173,'sinok','한문학과','$2y$10$pvvI1.iY8aGB6duf6zOfpuiha5nn/aSEv2LO5KPT8G3fPODXGmDRS','1986-03-02');
+/*!40000 ALTER TABLE `my_user_table` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-10-16 11:59:10
